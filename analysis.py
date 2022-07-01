8d44ce35ca2ddc1e614f6f7e87654e5bc55b6d40
rottaj <jackrotta12@gmail.com>
2022-06-30 20:18:42 -0400

    Updated dates

--- analysis.py
+++ analysis.py
@@ -8,31 +8,20 @@ if __name__ == "__main__":
   dfBtcDaily = pd.read_csv(r'./data/BTC-Daily.csv')
   dfBtcTweets = pd.read_csv(r'./data/BTC-Tweets.csv')
   #dfTweetsCleaned = pd.to_datetime(dfBtcTweets.date.astype(str), format="%Y-%m-%d %H:%M:%S").sort_values().to_numpy()
-  dfBtcTweets.sort_values(by="date")
-  print(dfBtcTweets.iloc[0], dfBtcTweets.iloc[-1])
-  
 
-  dfBtcDaily.set_index('date', inplace=True)
-  dfBtcDaily[dfBtcTweets[0]:dfBtcTweets[-1]]
+ 
+  z1 = dfBtcDaily.index[dfBtcDaily.date == "2021-07-01 00:00:00"].astype('int')
+  print(z1) # Returns 243
+  dfBtcDailyCleaned = dfBtcDaily[0: 243]
+  print(dfBtcDailyCleaned)
 
 
-  '''
-  plt.plot(df['date'].iloc[::-1], df['high'].iloc[::-1])
-  #plt.xticks(np.arange(min(df['date']), max(df['date'])), rotation='vertical')
-  locs, labels = plt.xticks();
-  print(locs, labels)
-  plt.ylabel("Highest Price")
-  plt.xlabel("Date")
-  plt.title("Bitcoin Price from Novemeber 2014 - March 2022")
-  '''
-
-  '''
   fig, (ax1, ax2) = plt.subplots(1, 2)
   fig.set_size_inches(10, 10)
-  ax1.plot(dfBtcDaily['date'].iloc[::-1], dfBtcDaily['high'].iloc[::-1])
+  ax1.plot(dfBtcDailyCleaned['date'].iloc[::-1], dfBtcDailyCleaned['high'].iloc[::-1])
   ax1.set_ylabel("Highest Price")
   ax1.set_xlabel("Date")
-  ax1.set_title("Bitcoin Price from Novemeber 2014 - March 2022")
+  fig.set_title("Bitcoin Price from July 2021 - March 2022")
 
   ax2.plot(
     pd.to_datetime(dfBtcTweets.date.astype(str),
@@ -40,7 +29,7 @@ if __name__ == "__main__":
     dfBtcTweets.user_followers)
  
   figTwo, ax3 = plt.subplots()
-  ax3.plot(dfBtcDaily['date'].iloc[::-1], dfBtcDaily['high'].iloc[::-1], color="blue")
+  ax3.plot(dfBtcDailyCleaned['date'].iloc[::-1], dfBtcDailyCleaned['high'].iloc[::-1], color="blue")
   ax4 = ax3.twinx()
   ax4.plot(
     pd.to_datetime(dfBtcTweets.date.astype(str),
@@ -50,4 +39,3 @@ if __name__ == "__main__":
   #print(pd.to_datetime(dfBtcTweets.date.astype(str),format="%Y-%m-%d %H:%M:%S").sort_values())
   #print(
   plt.show()
-  '''
